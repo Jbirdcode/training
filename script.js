5652ba14e52723088cca6b95af5a570abced6ae2
unknown <justin.wollard@gmail.com>
2024-04-24 20:55:28 -0400

    fixed changeBackground function

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 
 function changeBackground() {
     let body = document.body
-    let backgroundColor = window.getComputedStyle(body).getPropertyValue("background-image")
-    console.log(backgroundColor)
-    if (backgroundColor === "url(https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExejJ0M2hvczZ3aGd4YzE5NW9yODl2Zmw3dHI5MHgxbGdncjJlOXZ5YyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/r2DZ7c7zrY1jO/giphy.gif)" 
-        {
-        console.log("check")
-        body.style.backgroundColor = "rgb(52, 235, 134)"
+    let backgroundImage = window.getComputedStyle(body).getPropertyValue("background-image")
+    console.log(backgroundImage)
+    if (backgroundImage === "url(\"https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExejJ0M2hvczZ3aGd4YzE5NW9yODl2Zmw3dHI5MHgxbGdncjJlOXZ5YyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/r2DZ7c7zrY1jO/giphy.gif\")") {
+        console.log(backgroundImage)
+        body.style.backgroundImage = "url(\"https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExcjg1dHRtYzQwMTh6bHg4aGR3ZzlwYmlhdHh2MHIycjQ4cm1xZXd2bSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/WWYSFIZo4fsLC/giphy.gif\")"
     } else {
-        body.style.backgroundColor = " url(https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExejJ0M2hvczZ3aGd4YzE5NW9yODl2Zmw3dHI5MHgxbGdncjJlOXZ5YyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/r2DZ7c7zrY1jO/giphy.gif)"
+        body.style.backgroundImage = "url(\"https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExejJ0M2hvczZ3aGd4YzE5NW9yODl2Zmw3dHI5MHgxbGdncjJlOXZ5YyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/r2DZ7c7zrY1jO/giphy.gif\")"
     }
 }
 
